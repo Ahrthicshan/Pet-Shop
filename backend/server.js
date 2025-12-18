17cc1a6c7d00d54ece3d4ad2558225e1cb6a35d3
Ahrthicshan <ahrthivaseeharan@gmail.com>
2025-12-18 12:45:54 +0530

    Fix formatting in server.js

--- backend/server.js
+++ backend/server.js
@@ -17,14 +17,14 @@ mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/petshop', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
 app.get('/', (req, res) => {
     res.send('Petshop API is running');
 });
-
+//
 // Start Server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
